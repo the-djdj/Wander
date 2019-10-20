fc07a2dcb97e68bcd34f9c4ed3cd6084a5838f8e
Dan Jenkins <jenkins.daniel.02@gmail.com>
2019-10-20 22:22:17 +0200

    [new] Added basic classes.

--- wander-py/core.py
+++ wander-py/core.py
@@ -1,4 +1,20 @@
+from util import Output
+
+class Main:
+    ''' The main class. This holds all of the methods used in building the
+        wander system.'''
+
+
+    def __init__(self):
+        ''' The constructor. This creates the main object, and sets all of the
+            default variables.'''
+        # Create the output system
+        self.output = Output()
+
+
+
 if __name__ == '__main__':
     ''' The entry point into the wander-py application. This starts the whole
         process, so that things can run smoothly.'''
-    pass
+    # Create the build environment
+    main = Main()
---
+++ wander-py/util.py
@@ -0,0 +1,11 @@
+class Output:
+    ''' The output class. This creates a new object for managing the output,
+        and provides an array of useful methods for console output.'''
+
+
+    def __init__(self):
+        ''' The constructor. This assigns the default values used in the\
+            object.'''
+        pass
+
+
